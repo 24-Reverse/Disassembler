1fc7c284549fb0bf5365b5934f0de6f3b76d627b
mono <monologue1024@outlook.com>
2024-06-09 10:45:14 +0800

    Add function to extract PE file information

--- disassembler.py
+++ disassembler.py
@@ -27,6 +27,16 @@ class Disassembler:
         arch: 指令集,默认为x86
         mode: 寻址模式,默认64位
         '''
+        self.hash = None
+        self.header_info = None
+        self.sectons = None
+        self.import_table = None
+        self.export_table = None
+        self.got = None
+        self.plt = None
+        self.str_info = None
+        self.thrird_party_lib = None
+
         # 反汇编时使用
         self.mode = Cs(arch, mode)
         self.binary = lief.parse(binary_path)
@@ -45,15 +55,40 @@ class Disassembler:
             - 第三方库信息
         '''
         # TODO: 提取二进制文件属性
-        # Just for example, feel free to modify
-        self.hash = None
-        self.header_info = None
-        self.import_table = None
-        self.export_table = None
-        self.got = None
-        self.plt = None
-        self.str_info = None
-        self.thrird_party_lib = None
+        binary = self.binary
+        if isinstance(binary, lief.PE.Binary):
+            # PE 文件
+            # 获取文件的哈希值
+            self.hash = {
+                "md5": binary.authentihash_md5,
+                "sha1": binary.authentihash_sha1,
+                "sha256": binary.authentihash_sha256
+            }
+
+            # 头字段信息
+            self.header_info = {
+                "dos_header": binary.dos_header,            # DOS 头
+                "file_header": binary.header,               # 文件头
+                "optional_header": binary.optional_header   # 可选头
+            }
+
+            # 节信息
+            self.sectons = binary.sections
+
+            # 导入表
+            if binary.has_imports:
+                self.import_table = binary.imports
+
+            # 导出表
+            if binary.has_exports:
+                self.export_table = binary.get_export()
+
+            # 字符串信息
+            # ！！！未测试！！！
+            if binary.has_resources:
+                manager = binary.resources_manager
+                if manager.has_string_table:
+                    self.str_info = manager.string_table
         
     def disassemble_section(self):
         '''
@@ -79,4 +114,86 @@ class Disassembler:
         将分析的信息写入xml文件
         '''
         # TODO:写入xml文件
-        
+
+    def show_info(self):
+        '''
+        打印二进制文件属性
+        '''
+        binary = self.binary
+        if isinstance(binary, lief.PE.Binary):
+            # 获取文件的哈希值
+            hash_info = self.hash
+            print("======================================================")
+            print("Hash info")
+            print("======================================================\n")
+            print(f"md5:    {hash_info['md5']}")
+            print(f"sha1:   {hash_info['sha1']}")
+            print(f"sha256: {hash_info['sha256']}\n")
+
+            # 文件头字段信息
+            # DOS 头
+            dos_header = self.header_info["dos_header"]
+            print("======================================================")
+            print("DOS Header")
+            print("======================================================\n")
+            print(dos_header)
+
+            # 文件头
+            file_header = self.header_info["file_header"]
+            print("======================================================")
+            print("File Header")
+            print("======================================================\n")
+            print(f"Machine:                    {file_header.machine}")
+            print(f"Number of sections:         {file_header.numberof_sections}")
+            print(f"Time Date Stamp:            {file_header.time_date_stamps}")
+            print(f"Pointer to Symbol Table:    {file_header.pointerto_symbol_table}")
+            print(f"Number of Symbols:          {file_header.numberof_symbols}\n")
+
+            # 可选头
+            optional_header = self.header_info["optional_header"]
+            print("======================================================")
+            print("Optional Header")
+            print("======================================================\n")
+            print(f"Magic:                      {optional_header.magic}")
+            print(f"Size of Code:               {hex(optional_header.sizeof_code)}")
+            print(f"Address of Entry Point:     {hex(optional_header.addressof_entrypoint)}")
+            print(f"Base of Code:               {hex(optional_header.baseof_code)}")
+            print(f"Base of Data:               {hex(optional_header.baseof_data)}")
+            print(f"Image Base:                 {hex(optional_header.imagebase)}\n")
+
+            # 节信息
+            print("======================================================")
+            print("Sections")
+            print("======================================================\n")
+            for section in self.sectons:
+                print(f"Section:            {section.name}")
+                print(f"Virtual Address:    {section.virtual_address}")
+                print(f"Size:               {section.size}")
+                print(f"Entropy:            {section.entropy}\n")
+
+            # 导入
+            has_imports = not (self.import_table is None)
+            if has_imports:
+                print("======================================================")
+                print("Import Table")
+                print("======================================================\n")
+                for import_entry in self.import_table:
+                    print(import_entry)
+
+            # 导出表
+            has_exports = not (self.export_table is None)
+            if has_exports:
+                print("======================================================")
+                print("Export Table")
+                print("======================================================\n")
+                for export_entry in self.export_table:
+                    print(export_entry)
+
+            # 字符串信息
+            has_str_info = not (self.str_info is None)
+            if has_str_info:
+                print("======================================================")
+                print("String Table")
+                print("======================================================\n")
+                for string in self.str_info:
+                    print(string)
--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 import disassembler
+
+
+if __name__ == "__main__":
+    disas = disassembler.Disassembler("bin/a.exe")
+    disas.extract_bin_info()
+    disas.show_info()
